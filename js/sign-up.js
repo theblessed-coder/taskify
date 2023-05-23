fa88316096cc07a32389d5571663505ad93099d8
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-05-23 20:01:09 +0100

    updated sign up form

---
+++ js/sign-up.js
@@ -0,0 +1,29 @@
+function validateForm(event) {
+  event.preventDefault();
+
+  // Reset error messages
+  document.getElementById("username-error").textContent = "";
+  document.getElementById("password-error").textContent = "";
+
+  // Get input values
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Validate username
+  if (username === "") {
+    document.getElementById("username-error").textContent =
+      "Username is required.";
+    return;
+  }
+
+  // Validate password
+  if (password === "") {
+    document.getElementById("password-error").textContent =
+      "Password is required.";
+    return;
+  }
+
+  // Success message
+  alert("Sign-up successful!");
+  document.getElementById("signup-form").reset();
+}
