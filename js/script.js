47bcc07df3d962d46f659f288005d6acf7233321
David Agor <agordavid14@gmail.com>
2023-05-23 10:56:59 -0700

    script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Function to toggle the visibility of the login form
+function toggleLoginForm() {
+    var loginForm = document.getElementById('login-page');
+    loginForm.classList.toggle('hidden');
+  }
+  
+  // Function to toggle the visibility of the signup form
+  function toggleSignupForm() {
+    var signupForm = document.getElementById('signup-page');
+    signupForm.classList.toggle('hidden');
+  }
