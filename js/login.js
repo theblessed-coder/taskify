dd913192a21084ab026328f8b06e9a1426805f4e
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-05-26 10:10:16 +0300

    Made changes to the login and sign up pages

--- js/login.js
+++ js/login.js
@@ -2,28 +2,28 @@ function validateForm(event) {
   event.preventDefault(); // Prevent form submission
 
   // Clear previous error messages
-  document.getElementById("usernameError").textContent = "";
-  document.getElementById("passwordError").textContent = "";
+  document.getElementById('usernameError').textContent = '';
+  document.getElementById('passwordError').textContent = '';
 
   // Get input values
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  const username = document.getElementById('username1').value;
+  const password = document.getElementById('password1').value;
 
   // Validate input fields
-  if (username.trim() === "") {
-    document.getElementById("usernameError").textContent =
-      "Username is required";
+  if (username.trim() === '') {
+    document.getElementById('usernameError').textContent =
+      'Username is required';
     return;
   }
 
-  if (password.trim() === "") {
-    document.getElementById("passwordError").textContent =
-      "Password is required";
+  if (password.trim() === '') {
+    document.getElementById('passwordError').textContent =
+      'Password is required';
     return;
   }
 
   // Perform login logic here
   // You can send the data to a server for authentication or perform client-side validation
   // For this example, we'll just display a success message
-  alert("Login successful!");
+  alert('Login successful!');
 }
