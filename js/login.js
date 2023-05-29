b83ea9f85f4e866c0308826758cf8c4f8bb6f0e9
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-05-29 14:09:32 +0100

    added new functionality

--- js/login.js
+++ js/login.js
@@ -2,28 +2,40 @@ function validateForm(event) {
   event.preventDefault(); // Prevent form submission
 
   // Clear previous error messages
-  document.getElementById('usernameError').textContent = '';
-  document.getElementById('passwordError').textContent = '';
+  document.getElementById("usernameError").textContent = "";
+  document.getElementById("passwordError").textContent = "";
 
   // Get input values
-  const username = document.getElementById('username1').value;
-  const password = document.getElementById('password1').value;
+  const username = document.getElementById("username1").value;
+  const password = document.getElementById("password1").value;
 
   // Validate input fields
-  if (username.trim() === '') {
-    document.getElementById('usernameError').textContent =
-      'Username is required';
+  if (username.trim() === "") {
+    document.getElementById("usernameError").textContent =
+      "Username is required";
     return;
   }
 
-  if (password.trim() === '') {
-    document.getElementById('passwordError').textContent =
-      'Password is required';
+  if (password.trim() === "") {
+    document.getElementById("passwordError").textContent =
+      "Password is required";
     return;
   }
 
-  // Perform login logic here
-  // You can send the data to a server for authentication or perform client-side validation
-  // For this example, we'll just display a success message
-  alert('Login successful!');
+  // Simulated user credentials
+  const userCredentials = {
+    username: "exampleuser",
+    password: "examplepassword",
+  };
+
+  // Check if credentials match
+  if (
+    username.trim() === userCredentials.username &&
+    password.trim() === userCredentials.password
+  ) {
+    // Redirect to dashboard upon successful login
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid username or password. Please try again.");
+  }
 }
